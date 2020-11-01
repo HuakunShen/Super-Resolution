149d8b656345c1f58db3b5326be30cce7616d207
Huakun Shen <shenhuakun@outlook.com>
2020-11-01 14:02:07 -0500

    Update scale.py to support both interpolation scaling and regular scaling

--- preprocessing/scale.py
+++ preprocessing/scale.py
@@ -19,14 +19,20 @@ parser.add_argument('-o', '--output', type=str,
 parser.add_argument('-e', '--ext',
                     help='file extension, for example, your input is a directory and you only want to '
                          'scale all the .png files')
+parser.add_argument('--interpolate', action='store_true',
+                    help='Use this option to indicate whether you want to use interpolation for scaling')
 
 
-def scale(image, factor: float, down=True):
+def scale(image, factor: float, down=True, interpolation=True):
+    print("down" if down else "up")
     if down:
         factor = 1.0 / factor
     width = int(image.shape[1] * factor)
     height = int(image.shape[0] * factor)
-    resized = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+    if interpolation:
+        resized = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+    else:
+        resized = cv2.resize(image, (width, height))
     return resized
 
 
@@ -57,20 +63,28 @@ if __name__ == '__main__':
     if os.path.isfile(args.input):
         img = cv2.imread(args.input, cv2.IMREAD_UNCHANGED)
         print('Original Dimensions : ', img.shape)
-        output_img = scale(img, args.factor, args.mode == Mode.DOWN)
+        output_img = scale(img, args.factor, args.mode == Mode.DOWN, interpolation=args.interpolate)
         print('Resized Dimensions : ', output_img.shape)
         cv2.imwrite(args.output, output_img)
     elif os.path.isdir(args.input):
         all_files = os.listdir(args.input)
         if args.ext:
             all_files = list(filter(lambda f: f.endswith(args.ext), all_files))
+        output_dir = os.path.abspath(args.output)
+        if not os.path.exists(output_dir):
+            print("output dir DNE, creating it")
+            os.makedirs(output_dir)
         for file in all_files:
-            file_path = os.path.join(args.output, file)
+            file_path = os.path.join(args.input, file)
             img = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
+            if img is None:
+                raise ValueError("Read img is none, filename: {}".format(file_path))
             print("file: {}".format(file))
             print('Original Dimensions : ', img.shape)
-            output_img = scale(img, args.factor, args.mode == Mode.UP)
+            output_img = scale(img, args.factor, args.mode == Mode.DOWN, interpolation=args.interpolate)
             print('Resized Dimensions : ', output_img.shape)
-            output_img.imwrite(os.path.join(args.output, file), output_img)
+            output_path = os.path.join(os.path.abspath(args.output), file)
+            print("output path: {}".format(output_path))
+            cv2.imwrite(output_path, output_img)
     else:
         print("Unknown Error")
